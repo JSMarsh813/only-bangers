2aa5b9abf104aec44b70e94fc09f2805042d166a
Janet Spellman <janetspellman13@gmail.com>
2025-02-20 21:27:44 -0800

    still working on signup logic

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,7 +1,7 @@
 import { createSessionClient } from "@/appwrite/config";
 import { cookies } from "next/headers";
-import { databases } from "../../utils/appwrite";
-import { ID } from "appwrite";
+import { databases } from "../../../utils/appwrite"
+import { ID } from "appwrite"
 
 export async function GET(request) {
   //   const sessionCookie = cookies().get("session");
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,23 +1,77 @@
-"use client";
-import useAuth from "@/context/useAuth";
-import { useRouter } from "next/navigation";
+
 import React from "react";
-import Signup from "@/components/Signup";
+import { ID } from "node-appwrite";
+import { createAdminClient } from "../../appwrite/config"
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import auth from "../../partials/auth"
+// import {
+//   getLoggedInUser
+// } from "../../appwrite"
 
-const SignupPage = () => {
-  const router = useRouter();
-  const { authStatus } = useAuth();
+// const SignupPage = () => {
+//   const router = useRouter();
+//   const { authStatus } = useAuth();
 
-  if (authStatus) {
-    router.replace("/profile");
-    return <></>;
-  }
+//   if (authStatus) {
+//     router.replace("/profile");
+//     return <></>;
+//   }
+// src/app/signup/page.jsx
+
+
+async function signUpWithEmail(formData) {
+  "use server";
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const name = formData.get("name");
+
+  const { account } = await createAdminClient();
+  //createAdminClient has the permissions to create a new account
+
+  await account.create(ID.unique(), email, password, name);
+  const session = await account.createEmailPasswordSession(email, password);
+
+  //error: Error: Route "/signup" used `cookies().set('my-custom-session', ...)`. `cookies()` should be awaited before using its value. Learn more: https://nextjs.org/docs/messages/sync-dynamic-apis
+cookies().set("my-custom-session", session.secret, {
+    path: "/",
+    httpOnly: true,
+    sameSite: "strict",
+    secure: true,
+  });
+ 
+
+}
+
+export default async function SignUpPage() {
+  const user = await auth.getUser();
+  if (user) redirect("/account");
 
   return (
-    <section className="px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
-      <Signup />
-    </section>
+    <>
+      <form action={signUpWithEmail}>
+        <input
+          id="email"
+          name="email"
+          placeholder="Email"
+          type="email"
+        />
+        <input
+          id="password"
+          name="password"
+          placeholder="Password"
+          minLength={8}
+          type="password"
+        />
+        <input
+          id="name"
+          name="name"
+          placeholder="Name"
+          type="text"
+        />
+        <button type="submit">Sign up</button>
+      </form>
+    </>
   );
-};
-
-export default SignupPage;
+}
