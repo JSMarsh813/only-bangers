7fa17632e2a15d72b3bc2d53c7df858baed49a63
Janet Spellman <janetspellman13@gmail.com>
2025-06-21 15:55:54 -0700

    bug, email was not correctly taken out of the formData Object, waiting on api endpoint rate limit to refresh to confirm fix

--- src/server-actions/emailResetPasswordLink.tsx
+++ src/server-actions/emailResetPasswordLink.tsx
@@ -10,13 +10,24 @@ export async function createRecoveryPassword(
   formData: FormData,
 ): Promise<{ messageToUser: string } | null> {
   // if you look at the return statements below, you see we're returning objects with a messageToUser property
+  // State: {"messageToUser":"If  exists in our database an email has been sent for account recovery. If you do not see the email within a few minutes, please check your spam folder"}
+
   //since react 19 useActionState sends data from forms as a formdata object we use the FormData type
 
   //we're returning a promise with 2 possible types
 
   const objectFromForm = Object.fromEntries(formData);
-  const emailStringFormDataEntryType = Object.values(objectFromForm)[0];
+
+  const emailStringFormDataEntryType = objectFromForm["email"];
+  console.log(
+    `this is emailStringFormDataEntryType ${JSON.stringify(
+      emailStringFormDataEntryType,
+    )} typeof ${typeof emailStringFormDataEntryType}`,
+  );
   const emailString: string = emailStringFormDataEntryType as string;
+  console.log(
+    `this is emailString ${emailString} typeof ${typeof emailString}`,
+  );
   //forDataEntryValues can either be files or strings, in this case we're sure that it will be a string so I'm just doing type assertion
   //however if a file was sent instead of a string, this could lead to runtime errors
   //we need to convert it to a string because AppWrite's account.CreateRecovery requires 2 string values
