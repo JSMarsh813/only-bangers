72aaa5f0eaab5cacbab30782bbdbc741be1c4e82
Janet Spellman <janetspellman13@gmail.com>
2025-06-29 14:28:39 -0700

    refactoring: converted required span to tsx

--- src/app/components/form/RequiredSpan.tsx
+++ src/app/components/form/RequiredSpan.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function RequiredSpan(text) {
+export default function RequiredSpan() {
   return (
     <span className="bg-red-700 font-bold text-white block w-32 my-4 mx-auto px-2">
       Required *
