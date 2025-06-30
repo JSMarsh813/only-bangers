7fcc06a4bda75cadfc8a55cba36874917b0133fa
Janet Spellman <janetspellman13@gmail.com>
2025-06-29 20:15:57 -0700

    feat: updated dashboard to get user name from context and show username in welcome section

--- src/app/components/dashboard/DashboardGreetingUserSection.jsx
+++ src/app/components/dashboard/DashboardGreetingUserSection.jsx
@@ -1,12 +1,17 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import header from "../../../../public/space.jpg";
-import { Suspense } from "react";
+
+import { useUser } from "../context-wrappers/UserInfo";
 
 export default function DashboardGreetingUserSection({
   likedGeneralPostsCount,
   submittedGeneralPostsCount,
 }) {
+  const { currentUsersInfo, setTriggerRecheck, triggerRecheck } = useUser();
+
   return (
     <div className=" h-[198px] w-full relative ">
       <Image
@@ -20,7 +25,9 @@ export default function DashboardGreetingUserSection({
       />
       <div className="absolute w-11/12 sm:w-3/4 bg-opacity-80 top-10 bg-blue-950 mx-auto text-white  inset-0 font-extrabold h-[300px] border-4 rounded-3xl p-4 border-white">
         <h1 className="text-center text-2xl bg-blue-800 rounded-3xl py-4">
-          Welcome User!
+          {`Welcome ${
+            currentUsersInfo?.user_name || "error! no username found"
+          }!`}
         </h1>
         <h4> Stats </h4>
 
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,7 @@
 "use server";
 
-import { createSession } from "@/partials/auth";
-import { redirect } from "next/navigation";
-import { getUser } from "../../partials/auth";
-import { useUser } from "../components/context-wrappers/UserInfo";
-import LostPasswordLogin from "../components/form/LostPasswordForm";
 import LoginForm from "../components/form/LoginForm";
-import GeneralButton from "../components/GeneralButton";
+
 import Link from "next/link";
 import Image from "next/image";
 export default async function () {
