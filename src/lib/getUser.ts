a9af17880571f77bbdc468c4dc56b0c02fb97e7d
Janet Spellman <janetspellman13@gmail.com>
2025-07-01 02:14:01 -0700

    refactored: changed getUser to typescript, working on updating the paths to the lib folder

--- src/lib/getUser.ts
+++ src/lib/getUser.ts
@@ -3,20 +3,33 @@ import { redirect } from "next/navigation";
 import { ID } from "node-appwrite";
 import { createNewUser } from "../server-actions/createUser";
 import { createAdminClient, createSessionClient } from "@/lib/appwrite/config";
+import { Account, Models } from "appwrite";
 
-export default async function getUser() {
+export default async function getUser(): Promise<Models.User<Models.Preferences> | null> {
   console.log("get User ran");
-  let auth = {};
+
   const cookieStore = await cookies();
-  auth.sessionCookie = cookieStore.get("session");
+  const sessionCookie = cookieStore.get("session");
   // console.log(`this is auth session cookieStore ${ JSON.stringify(auth.sessionCookie)}`)
+  const cookieValue = sessionCookie?.value;
+
+  let user: Models.User<Models.Preferences> | null = null;
+
   try {
-    const { account } = await createSessionClient(auth.sessionCookie.value);
-    auth.user = await account.get();
+    if (!cookieValue) {
+      console.log("No session cookie found.");
+      return null;
+    }
+    const { account } = await createSessionClient(sessionCookie.value);
+    user = await account.get();
     // console.log(`this is auth user ${JSON.stringify(auth.user)}`)
   } catch {
-    auth.user = null;
-    auth.sessionCookie = null;
+    user = null;
+    console.log(
+      `user account was not found, there ${
+        cookieValue ? "was" : "wasn't"
+      } a session cookie value`,
+    );
   }
-  return auth.user;
+  return user;
 }
